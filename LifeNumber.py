65ece7d3b160d44975d94dbce477e6908df0ef13
soyung0951 <dgsw0951@dgsw.hs.kr>
2025-06-09 11:40:20 +0900

    Add files via upload

--- LifeNumber.py
+++ LifeNumber.py
@@ -1,4 +1,4 @@
-def getNumber(num): # 선천수 계산
+def getNumber(num): # 선천수, 후천수, 직업수 계산
     numString = str(num)
     sum = 0
     for i in numString:
@@ -7,3 +7,6 @@ def getNumber(num): # 선천수 계산
         return sum
     else:
         return getNumber(sum)
+    
+def getCareerCard():
+    
